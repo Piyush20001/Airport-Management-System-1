af89f74e0d05c1453328f07f94d87c88320656eb
Piyush20001 <44575274+Piyush20001@users.noreply.github.com>
2019-06-01 09:09:44 +0530

    Update AMS.py

--- AMS.py
+++ AMS.py
@@ -4,7 +4,6 @@ import string
 import mysql.connector
 
 def randomString(stringLength=6):
-    """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
